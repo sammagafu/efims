032a0315c4886afbd0ba168261644d4f839f6853
sam <retina@code.local>
2020-08-18 16:10:41 +0300

    changed runtime

--- efims/settings.py
+++ efims/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # addion configurations
 
 LOGIN_REDIRECT_URL = '/'
